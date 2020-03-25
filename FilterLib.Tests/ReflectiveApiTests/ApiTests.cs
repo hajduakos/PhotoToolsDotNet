dfb5d61389b39e810303ffea2b2b4d409d6cd4ac
Hajdu Ákos <hajdua@mit.bme.hu>
2020-03-25 09:33:17 +0100

    Refactor reflective api

---
+++ FilterLib.Tests/ReflectiveApiTests/ApiTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace FilterLib.Tests.ReflectiveApiTests
+{
+    public class ApiTests
+    {
+        [Test]
+        public void TestListFilters() =>
+            Assert.AreEqual(31, ReflectiveApi.GetFilterTypes().ToArray().Length);
+
+        [Test]
+        public void TestListBlends() =>
+            Assert.AreEqual(6, ReflectiveApi.GetBlendTypes().ToArray().Length);
+
+    }
+}
--- FilterLib/ReflectiveApi.cs
+++ FilterLib/ReflectiveApi.cs
@@ -4,21 +4,43 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Reflection;
+using System.Linq;
 
 namespace FilterLib
 {
+    /// <summary>
+    /// A collection of functions related to querying, constructing and manipulating
+    /// filters and blends via reflection.
+    /// </summary>
     public static class ReflectiveApi
     {
+        /// <summary>
+        /// List all filter types.
+        /// </summary>
+        /// <returns>Filter types</returns>
+        public static IEnumerable<Type> GetFilterTypes() =>
+            Assembly.GetExecutingAssembly().GetTypes().Where(t => t.GetCustomAttributes(typeof(FilterAttribute), false).Length > 0);
+
+        /// <summary>
+        /// Get a filter by its name. Throws an exception if not found.
+        /// </summary>
+        /// <param name="name">Name</param>
+        /// <returns>Filter type</returns>
         private static Type GetFilterTypeByName(string name)
         {
-            foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
-                if (type.GetCustomAttributes(typeof(FilterAttribute), false).Length > 0 &&
-                    (type.Name == name || type.Name == name + "Filter"))
+            foreach (Type type in GetFilterTypes())
+                if (type.Name == name || type.Name == name + "Filter")
                     return type;
 
             throw new ArgumentException("Filter '" + name + "' not found.");
         }
 
+        /// <summary>
+        /// Construct a filter by its name. Throws an exception if not found
+        /// or no parameterless constructor is available.
+        /// </summary>
+        /// <param name="name">Filter name</param>
+        /// <returns>Filter instance</returns>
         public static IFilter ConstructFilterByName(string name)
         {
             Type type = GetFilterTypeByName(name);
@@ -36,17 +58,37 @@ namespace FilterLib
 
             throw new ArgumentException("No parameterless constructor found for '" + name + "'.");
         }
+
+        /// <summary>
+        /// Get all filter properties for a filter.
+        /// </summary>
+        /// <param name="type">Filter type</param>
+        /// <returns>List of properties</returns>
+        public static IEnumerable<PropertyInfo> GetFilterProperties(Type type) =>
+            type.GetProperties().Where(p => p.GetCustomAttributes(typeof(FilterParamAttribute), false).Length > 0);
     
+        /// <summary>
+        /// Get a filter property by name. Throws an exception if not found.
+        /// </summary>
+        /// <param name="type">Filter type</param>
+        /// <param name="name">Property name</param>
+        /// <returns>Property info</returns>
         private static PropertyInfo GetFilterPropertyByName(Type type, string name)
         {
-            foreach (PropertyInfo pi in type.GetProperties())
-                if (pi.GetCustomAttributes(typeof(FilterParamAttribute), false).Length > 0 && pi.Name == name)
+            foreach (PropertyInfo pi in GetFilterProperties(type))
+                if (pi.Name == name)
                     return pi;
 
             throw new ArgumentException("Property '" + name + "' not found for '" + type.Name + "'.");
         }
 
-        private static object ParseParamValue(Type type, string value)
+        /// <summary>
+        /// Parse a string value to a given type. Throws exception if cannot be parsed.
+        /// </summary>
+        /// <param name="type">Type to be parsed</param>
+        /// <param name="value">Value as string</param>
+        /// <returns>Value as type</returns>
+        private static object ParseStringToType(Type type, string value)
         {
             NumberFormatInfo nfi = CultureInfo.InvariantCulture.NumberFormat;
             if (type == typeof(Int32)) return Convert.ToInt32(value);
@@ -55,6 +97,11 @@ namespace FilterLib
             throw new ArgumentException("Parsing type '" + type.Name + "' is not yet supported.");
         }
 
+        /// <summary>
+        /// Range check based on min/max attributes. Throws exception if out of range.
+        /// </summary>
+        /// <param name="pi">Filter property to be checked</param>
+        /// <param name="value">Value</param>
         private static void RangeCheck(PropertyInfo pi, object value)
         {
             foreach (var attr in pi.GetCustomAttributes<FilterParamMinAttribute>())
@@ -71,25 +118,48 @@ namespace FilterLib
                     throw new ArgumentOutOfRangeException("Parameter value " + value + " is greater than maximum (" + attr.Value + ")");
         }
 
+        /// <summary>
+        /// Set a filter property by name with a value by string. Throws exception if
+        /// property not found or if value cannot be parsed.
+        /// </summary>
+        /// <param name="filter">Filter instance</param>
+        /// <param name="name">Property name</param>
+        /// <param name="value">Value to be set</param>
         public static void SetFilterPropertyByName(IFilter filter, string name, string value)
         {
             PropertyInfo pi = GetFilterPropertyByName(filter.GetType(), name);
-            object valueObj = ParseParamValue(pi.PropertyType, value);
+            object valueObj = ParseStringToType(pi.PropertyType, value);
             RangeCheck(pi, valueObj);
             pi.SetValue(filter, valueObj);
         }
 
+        /// <summary>
+        /// Get all blend types.
+        /// </summary>
+        /// <returns>List of blends</returns>
+        public static IEnumerable<Type> GetBlendTypes() =>
+            Assembly.GetExecutingAssembly().GetTypes().Where(t => t.GetCustomAttributes(typeof(BlendAttribute), false).Length > 0);
 
+        /// <summary>
+        /// Get a blend by name. Throws exception if not found.
+        /// </summary>
+        /// <param name="name">Blend name</param>
+        /// <returns>Blend type</returns>
         private static Type GetBlendTypeByName(string name)
         {
-            foreach (Type type in Assembly.GetExecutingAssembly().GetTypes())
-                if (type.GetCustomAttributes(typeof(BlendAttribute), false).Length > 0 &&
-                    (type.Name == name || type.Name == name + "Blend"))
+            foreach (Type type in GetBlendTypes())
+                if (type.Name == name || type.Name == name + "Blend")
                     return type;
 
             throw new ArgumentException("Blend '" + name + "' not found.");
         }
 
+        /// <summary>
+        /// Construct a blend by its name. Throws an exception if not found
+        /// or no parameterless constructor is available.
+        /// </summary>
+        /// <param name="name">Blend name</param>
+        /// <returns>Blend instance</returns>
         public static IBlend ConstructBlendByName(string name)
         {
             Type type = GetBlendTypeByName(name);
