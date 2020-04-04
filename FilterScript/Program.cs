fabbc16e0cf44ebf4ba215248f948a6bf273562f
Hajdu Ákos <hajdua@mit.bme.hu>
2020-04-04 10:50:45 +0200

    Parse input arguments with flags

--- FilterScript/Program.cs
+++ FilterScript/Program.cs
@@ -1,4 +1,5 @@
 ﻿using FilterScript.Model;
+using System;
 using System.Drawing;
 using System.IO;
 
@@ -8,11 +9,29 @@ namespace FilterScript
     {
         static void Main(string[] args)
         {
-            Batch batch = Parser.Parse(File.ReadAllLines(args[0]));
-            using Bitmap input = new Bitmap(args[1]);
+            string scriptPath = ParseArg(args, "s");
+            string inputPath = ParseArg(args, "i");
+            string outputPath = ParseArg(args, "o");
+            Batch batch = Parser.Parse(File.ReadAllLines(scriptPath));
+            using Bitmap input = new Bitmap(inputPath);
             batch.InputTask.Input = input;
             using Bitmap output = batch.Execute();
-            output.Save(args[2]);
+            output.Save(outputPath);
+        }
+
+        static string ParseArg(string[] args, string flag)
+        {
+            flag = "-" + flag;
+            for(int i = 0; i < args.Length; ++i)
+            {
+                if (args[i] == flag)
+                {
+                    if (i == args.Length - 1) throw new ArgumentException($"Value not found for argument {flag}.");
+                    return args[i + 1];
+                }
+            }
+
+            throw new ArgumentException($"Argument {flag} not found.");
         }
     }
 }
