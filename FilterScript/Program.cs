211a105d4ab7acae9ab0d37b6ba039ba5c37441e
Hajdu Ákos <hajdua@mit.bme.hu>
2020-04-08 18:56:33 +0200

    Use image format based on extension

--- FilterScript/Program.cs
+++ FilterScript/Program.cs
@@ -24,6 +24,39 @@ namespace FilterScript
             batch.InputTask.Input = input;
             using Bitmap output = batch.Execute();
             output.Save(outputPath, ImageFormat.Bmp);
+            switch(new FileInfo(outputPath).Extension.ToLower())
+            {
+                case ".bmp":
+                    output.Save(outputPath, ImageFormat.Bmp);
+                    break;
+                case ".gif":
+                    output.Save(outputPath, ImageFormat.Gif);
+                    break;
+                case ".png":
+                    output.Save(outputPath, ImageFormat.Png);
+                    break;
+                case ".tiff":
+                    output.Save(outputPath, ImageFormat.Tiff);
+                    break;
+                case ".jpg":
+                case ".jpeg":
+                    EncoderParameters eps = new EncoderParameters(1) { Param = new EncoderParameter[] { new EncoderParameter(Encoder.Quality, 97L) } };
+                    if (GetEncoder(ImageFormat.Jpeg) == null) Console.WriteLine("NULL");
+                    output.Save(outputPath, GetEncoder(ImageFormat.Jpeg), eps);
+                    break;
+                default:
+                    Console.WriteLine("Unsupported output extension.");
+                    break;
+            }
+        }
+
+        private static ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+                if (codec.FormatID == format.Guid)
+                    return codec;
+            return null;
         }
 
         static string ParseArg(string[] args, string flag)
