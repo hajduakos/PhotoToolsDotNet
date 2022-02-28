c7b5099460f66b3c3c067dd4ad2a37916431d5f1
hajduakos <sonic18291@hotmail.com>
2022-02-28 22:28:23 +0000

    Update to .net 6.0

--- FilterScript.Test/test-all.sh
+++ FilterScript.Test/test-all.sh
@@ -3,8 +3,8 @@ declare -i RESULT=0
 for f in Scripts/*.txt
 do
 	echo -e "$f \c"
-	dotnet ../FilterScript/bin/Release/netcoreapp3.0/FilterScript.dll -s $f -i "Images/_input.bmp" -o "${f}.bmp"	
-	dotnet bin/Release/netcoreapp3.0/FilterScript.Test.dll "Images/$(basename -- $f).bmp" "${f}.bmp"
+	dotnet ../FilterScript/bin/Release/net6.0/FilterScript.dll -s $f -i "Images/_input.bmp" -o "${f}.bmp"	
+	dotnet bin/Release/net6.0/FilterScript.Test.dll "Images/$(basename -- $f).bmp" "${f}.bmp"
 	if [ $? = 0 ]; then
 		echo "OK"
 	else
