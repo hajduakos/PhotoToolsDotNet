1391bf5d2e152749057fa2b7cd56ad3ef97adf21
hajduakos <sonic18291@hotmail.com>
2022-06-18 23:34:03 +0100

    Make other tests parameterized

--- FilterLib.Tests/FilterTests/OtherTests.cs
+++ FilterLib.Tests/FilterTests/OtherTests.cs
@@ -1,50 +1,53 @@
-﻿using FilterLib.Filters.Other;
+﻿using FilterLib.Filters;
+using FilterLib.Filters.Other;
 using FilterLib.Util;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace FilterLib.Tests.FilterTests
 {
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
     public class OtherTests
     {
-        [Test]
-        public void TestConvertToPolar()
+        internal static IEnumerable<TestCaseData> Data()
         {
-            Assert.IsTrue(Common.CheckFilter("_input_eq.bmp", "ConvertToPolar_72.bmp", new ConvertToPolarFilter(72), 1));
+            yield return new TestCaseData("_input_eq.bmp", "ConvertToPolar_72.bmp", new ConvertToPolarFilter(72), 1);
+        
+            yield return new TestCaseData("_input.bmp", "_input.bmp",
+                new ConvolutionFilter(new Util.Conv3x3(0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0)), 1);
+            yield return new TestCaseData("_input.bmp", "Convolution_1_1_1_0_0_0_-1_-1_-1_2_30.bmp",
+                new ConvolutionFilter(new Util.Conv3x3(1, 1, 1, 0, 0, 0, -1, -1, -1, 2, 30)), 1);
+        
+            yield return new TestCaseData("_input_eq.bmp", "EquirectToStereo_100_0.bmp",
+                new EquirectangularToStereographicFilter(100, 0), 1);
+            yield return new TestCaseData("_input_eq.bmp", "EquirectToStereo_120_150.bmp",
+                new EquirectangularToStereographicFilter(120, 150), 1);
+        
+            yield return new TestCaseData("_input.bmp", "_input.bmp",
+                new WavesFilter(Size.Relative(1), Size.Absolute(0), WavesFilter.WaveDirection.Horizontal), 1);
+            yield return new TestCaseData("_input.bmp", "_input.bmp",
+                new WavesFilter(Size.Relative(.5f), Size.Absolute(0), WavesFilter.WaveDirection.Vertical), 1);
+            yield return new TestCaseData("_input.bmp", "Waves_50pct_9px_Horizontal.bmp",
+                new WavesFilter(Size.Relative(.5f), Size.Absolute(9), WavesFilter.WaveDirection.Horizontal), 1);
+            yield return new TestCaseData("_input.bmp", "Waves_10pct_16px_Vertical.bmp",
+                new WavesFilter(Size.Relative(.1f), Size.Absolute(16), WavesFilter.WaveDirection.Vertical), 1);
+        }
+        internal static IEnumerable<TestCaseData> Exceptions()
+        {
+            yield return new TestCaseData("_input.bmp", "_input.bmp",
+                new WavesFilter(Size.Absolute(0), Size.Relative(1), WavesFilter.WaveDirection.Horizontal), 1);
         }
 
         [Test]
-        public void TestConvolution()
-        {
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "_input.bmp",
-                new ConvolutionFilter(new Util.Conv3x3(0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0)), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "Convolution_1_1_1_0_0_0_-1_-1_-1_2_30.bmp",
-                new ConvolutionFilter(new Util.Conv3x3(1, 1, 1, 0, 0, 0, -1, -1, -1, 2, 30)), 1));
-        }
+        [TestCaseSource("Data")]
+        public void Test(string input, string expected, IFilter filter, int tolerance) =>
+            Assert.IsTrue(Common.CheckFilter(input, expected, filter, tolerance));
 
         [Test]
-        public void TestEquirectangularToStereographic()
-        {
-            Assert.IsTrue(Common.CheckFilter("_input_eq.bmp", "EquirectToStereo_100_0.bmp",
-                new EquirectangularToStereographicFilter(100, 0), 1));
-            Assert.IsTrue(Common.CheckFilter("_input_eq.bmp", "EquirectToStereo_120_150.bmp",
-                new EquirectangularToStereographicFilter(120, 150), 1));
-        }
-
-        [Test]
-        public void TestWaves()
-        {
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "_input.bmp",
-                new WavesFilter(Size.Relative(1), Size.Absolute(0), WavesFilter.WaveDirection.Horizontal), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "_input.bmp",
-                new WavesFilter(Size.Relative(.5f), Size.Absolute(0), WavesFilter.WaveDirection.Vertical), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "Waves_50pct_9px_Horizontal.bmp",
-                new WavesFilter(Size.Relative(.5f), Size.Absolute(9), WavesFilter.WaveDirection.Horizontal), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "Waves_10pct_16px_Vertical.bmp",
-                new WavesFilter(Size.Relative(.1f), Size.Absolute(16), WavesFilter.WaveDirection.Vertical), 1));
-            // Exceptions
-            Assert.Throws<ArgumentException>(() => Common.CheckFilter("_input.bmp", "_input.bmp",
-                new WavesFilter(Size.Absolute(0), Size.Relative(1), WavesFilter.WaveDirection.Horizontal), 1));
-        }
+        [TestCaseSource("Exceptions")]
+        public void TestEx(string input, string expected, IFilter filter, int tolerance) =>
+            Assert.Throws<ArgumentException>(() => Common.CheckFilter(input, expected, filter, tolerance));
     }
 }
