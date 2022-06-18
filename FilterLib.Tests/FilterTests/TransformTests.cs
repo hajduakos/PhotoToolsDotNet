d4f774c05ee9dffb70e1cb2b592b29ebad22d733
hajduakos <sonic18291@hotmail.com>
2022-06-18 23:43:09 +0100

    Make transform tests parametric

--- FilterLib.Tests/FilterTests/TransformTests.cs
+++ FilterLib.Tests/FilterTests/TransformTests.cs
@@ -1,77 +1,75 @@
-﻿using FilterLib.Filters.Transform;
+﻿using FilterLib.Filters;
+using FilterLib.Filters.Transform;
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using InterpolationMode = System.Drawing.Drawing2D.InterpolationMode;
 
 namespace FilterLib.Tests.FilterTests
 {
     public class TransformTests
     {
-        [Test]
-        public void TestCrop()
-        {
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "_input.bmp",
-                new CropFilter(Util.Size.Absolute(0), Util.Size.Absolute(0), Util.Size.Relative(1), Util.Size.Relative(1)), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "Crop_25pct_45px_50pct_40px.bmp",
-                new CropFilter(Util.Size.Relative(.25f), Util.Size.Absolute(45), Util.Size.Relative(.5f), Util.Size.Absolute(40)), 1));
-            // Exceptions
-            Assert.Throws<ArgumentException>(() => Common.CheckFilter("_input.bmp", "_input.bmp",
-                new CropFilter(Util.Size.Absolute(-1), Util.Size.Absolute(0), Util.Size.Relative(1), Util.Size.Relative(1)), 1));
-            Assert.Throws<ArgumentException>(() => Common.CheckFilter("_input.bmp", "_input.bmp",
-                new CropFilter(Util.Size.Absolute(0), Util.Size.Absolute(-1), Util.Size.Relative(1), Util.Size.Relative(1)), 1));
-            Assert.Throws<ArgumentException>(() => Common.CheckFilter("_input.bmp", "_input.bmp",
-                new CropFilter(Util.Size.Absolute(0), Util.Size.Absolute(0), Util.Size.Relative(0), Util.Size.Relative(1)), 1));
-            Assert.Throws<ArgumentException>(() => Common.CheckFilter("_input.bmp", "_input.bmp",
-                new CropFilter(Util.Size.Absolute(0), Util.Size.Absolute(0), Util.Size.Relative(1), Util.Size.Relative(0)), 1));
-        }
 
-        [Test]
-        public void TestFlipHorizontal() =>
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "FlipHorizontal.bmp", new FlipHorizontalFilter(), 0));
+        private const InterpolationMode itp = InterpolationMode.NearestNeighbor;
 
-        [Test]
-        public void TestFlipVertical() =>
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "FlipVertical.bmp", new FlipVerticalFilter(), 0));
-
-        [Test]
-        public void TestResize()
-        {
-            const System.Drawing.Drawing2D.InterpolationMode itp = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
-            string suffix = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "" : "_l";
-
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "_input.bmp",
-                new ResizeFilter(Util.Size.Relative(1), Util.Size.Relative(1), itp), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", $"Resize_200pct_30px_NN{suffix}.bmp",
-                new ResizeFilter(Util.Size.Relative(2), Util.Size.Absolute(30), itp), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", $"Resize_25pct_50pct_NN{suffix}.bmp",
-                new ResizeFilter(Util.Size.Relative(.25f), Util.Size.Relative(.5f), itp), 1));
-            // Exceptions
-            Assert.Throws<ArgumentException>(() => Common.CheckFilter("_input.bmp", "_input.bmp",
-                new ResizeFilter(Util.Size.Relative(0), Util.Size.Relative(1), itp), 1));
-            Assert.Throws<ArgumentException>(() => Common.CheckFilter("_input.bmp", "_input.bmp",
-                new ResizeFilter(Util.Size.Relative(1), Util.Size.Relative(0), itp), 1));
-        }
-
-        [Test]
-        public void TestRotate()
+        internal static IEnumerable<TestCaseData> Data()
         {
             string suffix = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "" : "_l";
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "_input.bmp", new RotateFilter(0, false), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "_input.bmp", new RotateFilter(0, true), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", $"Rotate_30_false{suffix}.bmp", new RotateFilter(30, false), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", $"Rotate_30_true{suffix}.bmp", new RotateFilter(30, true), 1));
+
+            yield return new TestCaseData("_input.bmp",
+                new CropFilter(Util.Size.Absolute(0), Util.Size.Absolute(0), Util.Size.Relative(1), Util.Size.Relative(1)), 1);
+            yield return new TestCaseData("Crop_25pct_45px_50pct_40px.bmp",
+                new CropFilter(Util.Size.Relative(.25f), Util.Size.Absolute(45), Util.Size.Relative(.5f), Util.Size.Absolute(40)), 1);
+
+            yield return new TestCaseData("FlipHorizontal.bmp", new FlipHorizontalFilter(), 0);
+
+            yield return new TestCaseData("FlipVertical.bmp", new FlipVerticalFilter(), 0);
+
+            yield return new TestCaseData("_input.bmp",
+                new ResizeFilter(Util.Size.Relative(1), Util.Size.Relative(1), itp), 1);
+            yield return new TestCaseData($"Resize_200pct_30px_NN{suffix}.bmp",
+                new ResizeFilter(Util.Size.Relative(2), Util.Size.Absolute(30), itp), 1);
+            yield return new TestCaseData($"Resize_25pct_50pct_NN{suffix}.bmp",
+                new ResizeFilter(Util.Size.Relative(.25f), Util.Size.Relative(.5f), itp), 1);
+
+            yield return new TestCaseData("_input.bmp", new RotateFilter(0, false), 1);
+            yield return new TestCaseData("_input.bmp", new RotateFilter(0, true), 1);
+            yield return new TestCaseData($"Rotate_30_false{suffix}.bmp", new RotateFilter(30, false), 1);
+            yield return new TestCaseData($"Rotate_30_true{suffix}.bmp", new RotateFilter(30, true), 1);
+
+            yield return new TestCaseData("Rotate180.bmp", new Rotate180Filter(), 0);
+
+            yield return new TestCaseData("RotateLeft.bmp", new RotateLeftFilter(), 0);
+
+            yield return new TestCaseData("RotateRight.bmp", new RotateRightFilter(), 0);
+        }
+
+        internal static IEnumerable<TestCaseData> Exceptions()
+        {
+            yield return new TestCaseData("_input.bmp",
+                    new CropFilter(Util.Size.Absolute(-1), Util.Size.Absolute(0), Util.Size.Relative(1), Util.Size.Relative(1)), 1);
+            yield return new TestCaseData("_input.bmp",
+                new CropFilter(Util.Size.Absolute(0), Util.Size.Absolute(-1), Util.Size.Relative(1), Util.Size.Relative(1)), 1);
+            yield return new TestCaseData("_input.bmp",
+                new CropFilter(Util.Size.Absolute(0), Util.Size.Absolute(0), Util.Size.Relative(0), Util.Size.Relative(1)), 1);
+            yield return new TestCaseData("_input.bmp",
+                new CropFilter(Util.Size.Absolute(0), Util.Size.Absolute(0), Util.Size.Relative(1), Util.Size.Relative(0)), 1);
+
+            yield return new TestCaseData("_input.bmp",
+                new ResizeFilter(Util.Size.Relative(0), Util.Size.Relative(1), itp), 1);
+            yield return new TestCaseData("_input.bmp",
+                new ResizeFilter(Util.Size.Relative(1), Util.Size.Relative(0), itp), 1);
         }
 
         [Test]
-        public void TestRotate180() =>
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "Rotate180.bmp", new Rotate180Filter(), 0));
+        [TestCaseSource("Data")]
+        public void Test(string expected, IFilter filter, int tolerance) =>
+            Assert.IsTrue(Common.CheckFilter("_input.bmp", expected, filter, tolerance));
 
         [Test]
-        public void TestRotateLeft() =>
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "RotateLeft.bmp", new RotateLeftFilter(), 0));
-
-        [Test]
-        public void TestRotateRight() =>
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "RotateRight.bmp", new RotateRightFilter(), 0));
+        [TestCaseSource("Exceptions")]
+        public void TestEx(string expected, IFilter filter, int tolerance) =>
+            Assert.Throws<ArgumentException>(() => Common.CheckFilter("_input.bmp", expected, filter, tolerance));
     }
 }
