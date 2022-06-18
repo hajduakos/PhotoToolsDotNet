41c1023ba618b5ad2b57c300c12c0f4a6a73bbc1
hajduakos <sonic18291@hotmail.com>
2022-06-19 00:02:38 +0100

    Finish making filter tests parametric

--- FilterLib.Tests/FilterTests/MosaicTests.cs
+++ FilterLib.Tests/FilterTests/MosaicTests.cs
@@ -1,33 +1,34 @@
+using FilterLib.Filters;
 using FilterLib.Filters.Mosaic;
 using NUnit.Framework;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 namespace FilterLib.Tests.FilterTests
 {
+    [TestFixture]
+    [Parallelizable(ParallelScope.All)]
     public class MosaicTests
     {
-        [Test]
-        public void TestCrystallize()
+        internal static IEnumerable<TestCaseData> Data()
         {
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "Crystallize_10_100_0.bmp", new CrystallizeFilter(10, 100, 0), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "Crystallize_30_50_0.bmp", new CrystallizeFilter(30, 50, 0), 1));
+            string suffix = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "" : "_l";
+
+            yield return new TestCaseData("Crystallize_10_100_0.bmp", new CrystallizeFilter(10, 100, 0), 1);
+            yield return new TestCaseData("Crystallize_30_50_0.bmp", new CrystallizeFilter(30, 50, 0), 1);
+
+            yield return new TestCaseData($"Lego_16{suffix}.bmp", new LegoFilter(16), 1);
+            yield return new TestCaseData($"Lego_32{suffix}.bmp", new LegoFilter(32), 1);
+
+            yield return new TestCaseData("_input.bmp", new PixelateFilter(1), 1);
+            yield return new TestCaseData("Pixelate_10.bmp", new PixelateFilter(10), 1);
+            yield return new TestCaseData("Pixelate_25.bmp", new PixelateFilter(25), 1);
         }
 
         [Test]
-        public void TestLego()
-        {
-            string suffix = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "" : "l";
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", $"Lego_16{suffix}.bmp", new LegoFilter(16), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", $"Lego_32{suffix}.bmp", new LegoFilter(32), 1));
-        }
-
-        [Test]
-        public void TestPixelate()
-        {
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "_input.bmp", new PixelateFilter(1), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "Pixelate_10.bmp", new PixelateFilter(10), 1));
-            Assert.IsTrue(Common.CheckFilter("_input.bmp", "Pixelate_25.bmp", new PixelateFilter(25), 1));
-        }
+        [TestCaseSource("Data")]
+        public void Test(string expected, IFilter filter, int tolerance) =>
+            Assert.IsTrue(Common.CheckFilter("_input.bmp", expected, filter, tolerance));
     }
 }
 
