df1edfbe44a23b8937041bc457285fc4f83c7bef
hajduakos <sonic18291@hotmail.com>
2022-07-07 00:59:01 +0100

    Fix rgb in oil paint filter

--- FilterLib/Filters/Artistic/OilPaintFilter.cs
+++ FilterLib/Filters/Artistic/OilPaintFilter.cs
@@ -64,22 +64,22 @@ namespace FilterLib.Filters.Artistic
                                 // Calculate index (relative to current row)
                                 int idx = ySub * width_3 + xSub * 3;
                                 // Get luminance
-                                int avg = (int)(0.299 * rowOrig[idx + 2] + 0.587 * rowOrig[idx + 1] + 0.114 * rowOrig[idx]);
+                                int avg = (int)(0.299 * rowOrig[idx] + 0.587 * rowOrig[idx + 1] + 0.114 * rowOrig[idx + 2]);
                                 // Increase values
                                 intensities[avg]++;
                                 // Sum values
-                                red[avg] += rowOrig[idx + 2];
+                                red[avg] += rowOrig[idx];
                                 green[avg] += rowOrig[idx + 1];
-                                blue[avg] += rowOrig[idx];
+                                blue[avg] += rowOrig[idx + 2];
                             }
                         }
                         // Get the most frequent intensity
                         int max = 0;
                         for (int xSub = 0; xSub < 256; xSub++) if (intensities[max] < intensities[xSub]) max = xSub;
                         // First element of the row will be the average of the maximal intensity
-                        row[2] = (byte)(red[max] / intensities[max]);
+                        row[0] = (byte)(red[max] / intensities[max]);
                         row[1] = (byte)(green[max] / intensities[max]);
-                        row[0] = (byte)(blue[max] / intensities[max]);
+                        row[2] = (byte)(blue[max] / intensities[max]);
 
                         // Iterate through other columns
                         // Rather than building up the whole array again, we just take in another column
@@ -95,13 +95,13 @@ namespace FilterLib.Filters.Artistic
                                     // Calculate index (relative to current row)
                                     int idx = ySub * width_3 + x - radius_3 - 3;
                                     // Get luminance
-                                    int avg = (int)(0.299 * rowOrig[idx + 2] + 0.587 * rowOrig[idx + 1] + 0.114 * rowOrig[idx]);
+                                    int avg = (int)(0.299 * rowOrig[idx] + 0.587 * rowOrig[idx + 1] + 0.114 * rowOrig[idx + 2]);
                                     // Decrease values
                                     intensities[avg]--;
                                     // Sum values
-                                    red[avg] -= rowOrig[idx + 2];
+                                    red[avg] -= rowOrig[idx];
                                     green[avg] -= rowOrig[idx + 1];
-                                    blue[avg] -= rowOrig[idx];
+                                    blue[avg] -= rowOrig[idx + 2];
                                 }
                             }
                             // If we can add a new column
@@ -113,22 +113,22 @@ namespace FilterLib.Filters.Artistic
                                     // Calculate index (relative to current row)
                                     int idx = ySub * width_3 + x + radius_3;
                                     // Get luminance
-                                    int avg = (int)(0.299 * rowOrig[idx + 2] + 0.587 * rowOrig[idx + 1] + 0.114 * rowOrig[idx]);
+                                    int avg = (int)(0.299 * rowOrig[idx] + 0.587 * rowOrig[idx + 1] + 0.114 * rowOrig[idx + 2]);
                                     // Increase values
                                     intensities[avg]++;
                                     // Sum values
-                                    red[avg] += rowOrig[idx + 2];
+                                    red[avg] += rowOrig[idx];
                                     green[avg] += rowOrig[idx + 1];
-                                    blue[avg] += rowOrig[idx];
+                                    blue[avg] += rowOrig[idx + 2];
                                 }
                             }
                             // Get the most frequent intensity
                             max = 0;
                             for (int xSub = 0; xSub < 256; xSub++) if (intensities[max] < intensities[xSub]) max = xSub;
                             // Actual element will be the average of the maximal intensity
-                            row[x + 2] = (byte)(red[max] / intensities[max]);
+                            row[x] = (byte)(red[max] / intensities[max]);
                             row[x + 1] = (byte)(green[max] / intensities[max]);
-                            row[x] = (byte)(blue[max] / intensities[max]);
+                            row[x + 2] = (byte)(blue[max] / intensities[max]);
                         }
 
                         reporter?.Report(y, 0, image.Height - 1);
