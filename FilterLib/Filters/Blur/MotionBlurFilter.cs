e39b11538441345896145b56669705deaec097cd
hajduakos <sonic18291@hotmail.com>
2022-07-08 00:05:50 +0100

    Cleanup motion blur

--- FilterLib/Filters/Blur/MotionBlurFilter.cs
+++ FilterLib/Filters/Blur/MotionBlurFilter.cs
@@ -5,7 +5,8 @@ using MathF = System.MathF;
 namespace FilterLib.Filters.Blur
 {
     /// <summary>
-    /// Motion blur filter.
+    /// Apply motion blur by replacing each pixel with the averag
+    /// calculated along a line with a given angle and length.
     /// </summary>
     [Filter]
     public sealed class MotionBlurFilter : FilterInPlaceBase
@@ -32,7 +33,7 @@ namespace FilterLib.Filters.Blur
         public float Angle { get; set; }
 
         /// <summary>
-        /// Constructor with blur lenght and angle
+        /// Constructor.
         /// </summary>
         /// <param name="length">Blur length</param>
         /// <param name="angle">Blur angle in degrees</param>
@@ -43,54 +44,50 @@ namespace FilterLib.Filters.Blur
         }
 
         /// <inheritdoc/>
-        public override void ApplyInPlace(Image image, IReporter reporter = null)
+        public override unsafe void ApplyInPlace(Image image, IReporter reporter = null)
         {
             reporter?.Start();
             // Clone image (the clone won't be modified)
             Image original = (Image)image.Clone();
-            unsafe
+            System.Diagnostics.Debug.Assert(image.Width == original.Width);
+            int width_3 = image.Width * 3;
+            float sinAngle = MathF.Sin(Angle * MathF.PI / 180);
+            float cosAngle = MathF.Cos(Angle * MathF.PI / 180);
+
+            fixed (byte* newStart = image, oldStart = original)
             {
-                fixed (byte* start = image, origStart = original)
+                for (int y = 0; y < image.Height; ++y)
                 {
-                    int width_3 = image.Width * 3;
-                    float sinAngle = MathF.Sin(Angle * MathF.PI / 180);
-                    float cosAngle = MathF.Cos(Angle * MathF.PI / 180);
-                    int idx, rSum, gSum, bSum, n;
-                    // Iterate through rows
-                    for (int y = 0; y < image.Height; ++y)
+                    for (int x = 0; x < width_3; x += 3)
                     {
-                        // Iterate through columns
-                        for (int x = 0; x < width_3; x += 3)
+                        float bSum = 0, gSum = 0, rSum = 0;
+                        int n = 0;
+                        // Iterate through each pixel of the line
+                        for (int k = -length; k <= length; ++k)
                         {
-                            // We have to sum each component through a line
-                            bSum = gSum = rSum = n = 0;
-                            // Iterate through each pixel of the line
-                            for (int k = -length; k <= length; ++k)
+                            int dx = (int)MathF.Round(k * cosAngle);
+                            int dy = (int)MathF.Round(k * sinAngle);
+                            int x1 = x / 3 + dx;
+                            int y1 = y + dy;
+                            // Skip pixels outside the image
+                            if (x1 >= 0 && x1 < image.Width && y1 >= 0 && y1 < image.Height)
                             {
-                                int dx = (int)MathF.Round(k * cosAngle); // Horizontal distance from the center pixel
-                                int dy = (int)MathF.Round(k * sinAngle); // Vertical distance from the center pixel
-                                int xAct = x / 3 + dx; // x coord. of the actual pixel
-                                int yAct = y + dy; // y coord. of the actual pixel
-                                // If the pixel is in the bounds of the image
-                                if (xAct >= 0 && xAct < image.Width && yAct >= 0 && yAct < image.Height)
-                                {
-                                    idx = yAct * width_3 + xAct * 3;   // Calculate index
-                                    rSum += origStart[idx + 2]; // Sum red component
-                                    gSum += origStart[idx + 1]; // Sum blue component
-                                    bSum += origStart[idx];     // Sum green component
-                                    ++n; // Number of items
-                                }
+                                int oldIdx = y1 * width_3 + x1 * 3;
+                                rSum += oldStart[oldIdx];
+                                gSum += oldStart[oldIdx + 1];
+                                bSum += oldStart[oldIdx + 2];
+                                ++n;
                             }
-                            idx = y * width_3 + x; // Index of the center pixel
-                            // Calculate average
-                            start[idx + 2] = (byte)(rSum / n);
-                            start[idx + 1] = (byte)(gSum / n);
-                            start[idx] = (byte)(bSum / n);
                         }
-                        reporter?.Report(y, 0, image.Height - 1);
+                        int newIdx = y * width_3 + x;
+                        newStart[newIdx] = (byte)(rSum / n);
+                        newStart[newIdx + 1] = (byte)(gSum / n);
+                        newStart[newIdx + 2] = (byte)(bSum / n);
                     }
+                    reporter?.Report(y, 0, image.Height - 1);
                 }
             }
+
             reporter?.Done();
         }
     }
