5992a2a5e85a0be7531976704c1527bc760cd63c
hajduakos <sonic18291@hotmail.com>
2022-07-07 22:22:01 +0100

    Cleanup adaptive treshold

--- FilterLib/Filters/Artistic/AdaptiveTresholdFilter.cs
+++ FilterLib/Filters/Artistic/AdaptiveTresholdFilter.cs
@@ -3,7 +3,8 @@
 namespace FilterLib.Filters.Artistic
 {
     /// <summary>
-    /// Adaptive treshold filter.
+    /// Map each pixel to black or white based on an adaptive treshold: the average
+    /// intensity is calculated in a given radius and used as a treshold.
     /// </summary>
     [Filter]
     public sealed class AdaptiveTresholdFilter : FilterInPlaceBase
@@ -25,96 +26,83 @@ namespace FilterLib.Filters.Artistic
         }
 
         /// <summary>
-        /// Constructor with square size.
+        /// Constructor.
         /// </summary>
-        /// <param name="squareSize">Square size</param>
+        /// <param name="squareSize">Square size [1;...]</param>
         public AdaptiveTresholdFilter(int squareSize = 1) => SquareSize = squareSize;
 
         /// <inheritdoc/>
-        public override void ApplyInPlace(Image image, IReporter reporter = null)
+        public override unsafe void ApplyInPlace(Image image, IReporter reporter = null)
         {
             reporter?.Start();
             // Clone image (the clone won't be modified)
             Image original = (Image)image.Clone();
 
-            unsafe
+            fixed (byte* newStart = image, oldStart = original)
             {
-                fixed (byte* start = image, origStart = original)
+                System.Diagnostics.Debug.Assert(image.Width == original.Width);
+                int width_3 = image.Width * 3;
+                int sqSize_3 = sqSize * 3;
+
+                // For each pixel we have to calculate the average intensity in a given radius. To make this
+                // more efficient, we go row-by-row and use a moving window:
+                // - First we calculate the full window for the first pixel
+                // - But then in each step moving right, we drop one row from the left and add one at the right
+                for (int y = 0; y < image.Height; ++y)
                 {
-                    int width_3 = image.Width * 3;
-                    int sqSize_3 = sqSize * 3;
+                    byte* newRow = newStart + (y * width_3);
+                    byte* oldRow = oldStart + (y * width_3);
 
-                    // Iterate through rows
-                    for (int y = 0; y < image.Height; ++y)
+                    float sum = 0;
+                    int n = 0;
+
+                    // Calculate full window around first column of current row
+                    for (int xSub = 0; xSub <= sqSize && xSub < image.Width; ++xSub)
                     {
-                        // Get rows
-                        byte* row = start + (y * width_3);
-                        byte* rowOrig = origStart + (y * width_3);
-
-                        // Clear sum
-                        float sum = 0;
-                        int n = 0;
-
-                        // Calculate sum with the surrounding of the first column of the actual row
-                        for (int xSub = 0; xSub <= sqSize && xSub < image.Width; ++xSub) // Horizontal
+                        for (int ySub = y < sqSize ? -y : -sqSize; y + ySub < image.Height && ySub <= sqSize; ++ySub)
                         {
-                            // Vertically from -squareSize to +squareSize
+                            int idx = ySub * width_3 + xSub * 3;
+                            sum += RRatio * oldRow[idx] + GRatio * oldRow[idx + 1] + BRatio * oldRow[idx + 2];
+                            ++n;
+                        }
+                    }
+                    float avg = sum / n;
+                    // Treshold first column of current row
+                    float lum = RRatio * oldRow[0] + GRatio * oldRow[1] + BRatio * oldRow[2];
+                    newRow[0] = newRow[1] = newRow[2] = (byte)(avg < lum ? 255 : 0);
+
+                    // Iterate through other columns and update window
+                    for (int x = 3; x < width_3; x += 3)
+                    {
+                        // Remove a column from the left
+                        if (x / 3 - sqSize - 1 >= 0)
+                        {
                             for (int ySub = y < sqSize ? -y : -sqSize; y + ySub < image.Height && ySub <= sqSize; ++ySub)
                             {
-                                // Calculate index (relative to current row)
-                                int idx = ySub * width_3 + xSub * 3;
-                                // Add luminance
-                                sum += RRatio * rowOrig[idx] + GRatio * rowOrig[idx + 1] + BRatio * rowOrig[idx + 2];
+                                int idx = ySub * width_3 + x - sqSize_3 - 3;
+                                sum -= RRatio * oldRow[idx] + GRatio * oldRow[idx + 1] + BRatio * oldRow[idx + 2];
+                                --n;
+                            }
+                        }
+                        // Add column to right
+                        if (x / 3 + sqSize < image.Width)
+                        {
+                            for (int ySub = y < sqSize ? -y : -sqSize; y + ySub < image.Height && ySub <= sqSize; ++ySub)
+                            {
+                                int idx = ySub * width_3 + x + sqSize_3;
+                                sum += RRatio * oldRow[idx] + GRatio * oldRow[idx + 1] + BRatio * oldRow[idx + 2];
                                 ++n;
                             }
                         }
-                        // Get average and luminance
-                        float avg = sum / n;
-                        float lum = RRatio * rowOrig[0] + GRatio * rowOrig[1] + BRatio * rowOrig[2];
-                        // Treshold first element
-                        row[0] = row[1] = row[2] = (byte)(avg < lum ? 255 : 0);
-
-                        // Iterate through other columns
-                        // Rather than calculating the sum again, we just take in another column
-                        // (+squareSize) and remove a column (-squareSize-1)
-                        for (int x = 3; x < width_3; x += 3)
-                        {
-                            // If we can remove a column
-                            if (x / 3 - sqSize - 1 >= 0)
-                            {
-                                // Remove column (vertically from -squareSize to +squareSize)
-                                for (int ySub = y < sqSize ? -y : -sqSize; y + ySub < image.Height && ySub <= sqSize; ++ySub)
-                                {
-                                    // Calculate index (relative to current row)
-                                    int idx = ySub * width_3 + x - sqSize_3 - 3;
-                                    // Subtract luminance
-                                    sum -= RRatio * rowOrig[idx] + GRatio * rowOrig[idx + 1] + BRatio * rowOrig[idx + 2];
-                                    --n;
-                                }
-                            }
-                            // If we can add a new column
-                            if (x / 3 + sqSize < image.Width)
-                            {
-                                // Add column (vertically from -squareSize to +squareSize)
-                                for (int ySub = y < sqSize ? -y : -sqSize; y + ySub < image.Height && ySub <= sqSize; ++ySub)
-                                {
-                                    // Calculate index (relative to current row)
-                                    int idx = ySub * width_3 + x + sqSize_3;
-                                    // Add luminance
-                                    sum += RRatio * rowOrig[idx] + GRatio * rowOrig[idx + 1] + BRatio * rowOrig[idx + 2];
-                                    ++n;
-                                }
-                            }
-                            // Get average and luminance
-                            avg = sum / n;
-                            lum = RRatio * rowOrig[x] + GRatio * rowOrig[x + 1] + BRatio * rowOrig[x + 2];
-                            // Treshold element
-                            row[x] = row[x + 1] = row[x + 2] = (byte)(avg < lum ? 255 : 0);
-                        }
-                        reporter?.Report(y, 0, image.Height - 1);
+                        avg = sum / n;
+                        // Treshold element
+                        lum = RRatio * oldRow[x] + GRatio * oldRow[x + 1] + BRatio * oldRow[x + 2];
+                        newRow[x] = newRow[x + 1] = newRow[x + 2] = (byte)(avg < lum ? 255 : 0);
                     }
+                    reporter?.Report(y, 0, image.Height - 1);
                 }
             }
+
             reporter?.Done();
         }
     }
