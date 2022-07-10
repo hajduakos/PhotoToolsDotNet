e1096af4b05c5ff0b4636af51e0b193a0c91a027
hajduakos <sonic18291@hotmail.com>
2022-07-10 09:48:17 +0100

    Cleanup convert to polar filter

--- FilterLib/Filters/Other/ConvertToPolarFilter.cs
+++ FilterLib/Filters/Other/ConvertToPolarFilter.cs
@@ -4,7 +4,7 @@ using MathF = System.MathF;
 namespace FilterLib.Filters.Other
 {
     /// <summary>
-    /// Convert to polar filter.
+    /// Convert cartesian coordinates to polar to get a circular effect.
     /// </summary>
     [Filter]
     public sealed class ConvertToPolarFilter : FilterInPlaceBase
@@ -35,88 +35,76 @@ namespace FilterLib.Filters.Other
         public ConvertToPolarFilter(float phase = 0) => Phase = phase;
 
         /// <inheritdoc/>
-        public override void ApplyInPlace(Image image, IReporter reporter = null)
+        public override unsafe void ApplyInPlace(Image image, IReporter reporter = null)
         {
             reporter?.Start();
             // Clone image (the clone won't be modified)
             Image original = (Image)image.Clone();
 
-            unsafe
+            int width_3 = image.Width * 3;
+            float halfHeight = (image.Height - 1) / 2f;
+            float halfWidth = (image.Width - 1) / 2f;
+            float aspect = image.Height / (float)image.Width; // Correction to circle
+            float xMul = 1 / (2 * MathF.PI) * (image.Width - 1); // Multiplier to convert to polar
+            float yMul = 1 / halfHeight * (image.Height - 1); // Multiplier to convert to polar
+
+            fixed (byte* newStart = image, oldStart = original)
             {
-                fixed (byte* start = image, origStart = original)
+                for (int y = 0; y < image.Height; ++y)
                 {
-                    int w = image.Width, h = image.Height;
-                    int width_3 = image.Width * 3; // Width of a row
-                    int x, y;
-                    float xCorr, yCorr; // Corrected coordinates
-                    float halfHeight = (h - 1) / 2f;
-                    float halfWidth = (w - 1) / 2f;
-                    float r, fi; // Radius and angle
-                    float xMult = h / (float)w; // Correction to circle
-                    int x0, y0, x1, y1; // Indexes in the original image (bilinear interpolation with 4 points)
-                    float xOrg, yOrg; // Coordinates in the original image (float)
-                    float xFrac, yFrac; // Fractional parts of the coordinates
-                    float xMultiplier = (1 / (2 * MathF.PI) * (w - 1)); // Multiplier to convert to polar
-                    float yMultiplier = 1 / halfHeight * (h - 1); // Multiplier to convert to polar
-
-                    // Iterate through rows
-                    for (y = 0; y < h; ++y)
+                    byte* newRow = newStart + y * width_3;
+                    for (int x = 0; x < width_3; x += 3)
                     {
-                        // Get rows
-                        byte* row = start + (y * width_3);
-                        // Iterate through columns
-                        for (x = 0; x < width_3; x += 3)
-                        {
-                            // Get corrected coordinates
-                            xCorr = ((x / 3) - halfWidth) * xMult;
-                            yCorr = halfHeight - y;
-                            // Get radius and angle
-                            r = MathF.Sqrt(xCorr * xCorr + yCorr * yCorr);
-                            fi = (-MathF.Atan2(xCorr, yCorr) + MathF.PI + phase / 180f * MathF.PI);
+                        // Get corrected coordinates
+                        float xCorr = ((x / 3) - halfWidth) * aspect;
+                        float yCorr = halfHeight - y;
+                        // Get radius and angle
+                        float r = MathF.Sqrt(xCorr * xCorr + yCorr * yCorr);
+                        float fi = (-MathF.Atan2(xCorr, yCorr) + MathF.PI + phase / 180f * MathF.PI);
 
-                            if (fi >= 2 * MathF.PI) fi -= 2 * MathF.PI;
+                        if (fi >= 2 * MathF.PI) fi -= 2 * MathF.PI;
 
-                            // Get X coordinate and points for interpolation in the original image
-                            xOrg = fi * xMultiplier;
+                        // Get X coordinate and points for interpolation in the original image
+                        float xOrg = fi * xMul;
 
-                            x0 = (int)MathF.Floor(xOrg);
-                            xFrac = xOrg - x0;
-                            if (x0 >= w) x0 = w - 1;
-                            x0 *= 3;
+                        int x0 = (int)MathF.Floor(xOrg);
+                        float xFrac = xOrg - x0;
+                        if (x0 >= image.Width) x0 = image.Width - 1;
+                        x0 *= 3;
 
-                            x1 = (int)MathF.Ceiling(xOrg);
-                            if (x1 >= w) x1 = w - 1;
-                            x1 *= 3;
+                        int x1 = (int)MathF.Ceiling(xOrg);
+                        if (x1 >= image.Width) x1 = image.Width - 1;
+                        x1 *= 3;
 
-                            // Get Y coordinate and points for interpolation in the original image
-                            yOrg = r * yMultiplier;
+                        // Get Y coordinate and points for interpolation in the original image
+                        float yOrg = r * yMul;
 
-                            y0 = (int)MathF.Floor(yOrg);
-                            yFrac = yOrg - y0;
+                        int y0 = (int)MathF.Floor(yOrg);
+                        float yFrac = yOrg - y0;
 
-                            if (y0 >= h) y0 = h - 1;
+                        if (y0 >= image.Height) y0 = image.Height - 1;
 
-                            y1 = (int)MathF.Ceiling(yOrg);
-                            if (y1 >= h) y1 = h - 1;
+                        int y1 = (int)MathF.Ceiling(yOrg);
+                        if (y1 >= image.Height) y1 = image.Height - 1;
 
-                            // Interpolation for R,G,B components
-                            row[x] = (byte)(origStart[y0 * width_3 + x0] * (1 - xFrac) * (1 - yFrac)
-                                + origStart[y1 * width_3 + x0] * (1 - xFrac) * yFrac
-                                + origStart[y0 * width_3 + x1] * xFrac * (1 - yFrac)
-                                + origStart[y1 * width_3 + x1] * xFrac * yFrac);
-                            row[x + 1] = (byte)(origStart[y0 * width_3 + x0 + 1] * (1 - xFrac) * (1 - yFrac)
-                                + origStart[y1 * width_3 + x0 + 1] * (1 - xFrac) * yFrac
-                                + origStart[y0 * width_3 + x1 + 1] * xFrac * (1 - yFrac)
-                                + origStart[y1 * width_3 + x1 + 1] * xFrac * yFrac);
-                            row[x + 2] = (byte)(origStart[y0 * width_3 + x0 + 2] * (1 - xFrac) * (1 - yFrac)
-                                + origStart[y1 * width_3 + x0 + 2] * (1 - xFrac) * yFrac
-                                + origStart[y0 * width_3 + x1 + 2] * xFrac * (1 - yFrac)
-                                + origStart[y1 * width_3 + x1 + 2] * xFrac * yFrac);
-                        }
-                        reporter?.Report(y, 0, h - 1);
+                        // Bilinear interpolation
+                        newRow[x] = (byte)(oldStart[y0 * width_3 + x0] * (1 - xFrac) * (1 - yFrac)
+                            + oldStart[y1 * width_3 + x0] * (1 - xFrac) * yFrac
+                            + oldStart[y0 * width_3 + x1] * xFrac * (1 - yFrac)
+                            + oldStart[y1 * width_3 + x1] * xFrac * yFrac);
+                        newRow[x + 1] = (byte)(oldStart[y0 * width_3 + x0 + 1] * (1 - xFrac) * (1 - yFrac)
+                            + oldStart[y1 * width_3 + x0 + 1] * (1 - xFrac) * yFrac
+                            + oldStart[y0 * width_3 + x1 + 1] * xFrac * (1 - yFrac)
+                            + oldStart[y1 * width_3 + x1 + 1] * xFrac * yFrac);
+                        newRow[x + 2] = (byte)(oldStart[y0 * width_3 + x0 + 2] * (1 - xFrac) * (1 - yFrac)
+                            + oldStart[y1 * width_3 + x0 + 2] * (1 - xFrac) * yFrac
+                            + oldStart[y0 * width_3 + x1 + 2] * xFrac * (1 - yFrac)
+                            + oldStart[y1 * width_3 + x1 + 2] * xFrac * yFrac);
                     }
+                    reporter?.Report(y, 0, image.Height - 1);
                 }
             }
+
             reporter?.Done();
         }
     }
