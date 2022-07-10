19468a2b190e988429a0319c902dd1bf069d550f
hajduakos <sonic18291@hotmail.com>
2022-07-10 11:27:03 +0100

    Cleanup convolution filter

--- FilterLib/Filters/Other/ConvolutionFilter.cs
+++ FilterLib/Filters/Other/ConvolutionFilter.cs
@@ -4,7 +4,7 @@ using FilterLib.Util;
 namespace FilterLib.Filters.Other
 {
     /// <summary>
-    /// Convolution filter.
+    /// Convolution filter using a given 3x3 kernel (matrix).
     /// </summary>
     [Filter]
     public sealed class ConvolutionFilter : FilterInPlaceBase
@@ -22,49 +22,42 @@ namespace FilterLib.Filters.Other
         public ConvolutionFilter(Conv3x3 matrix = new Conv3x3()) => Matrix = matrix;
 
         /// <inheritdoc/>
-        public override void ApplyInPlace(Image image, IReporter reporter = null)
+        public override unsafe void ApplyInPlace(Image image, IReporter reporter = null)
         {
             reporter?.Start();
             // Clone image (the clone won't be modified)
             Image original = (Image)image.Clone();
-
-            unsafe
+            System.Diagnostics.Debug.Assert(original.Width == image.Width);
+            int width_3 = image.Width * 3;
+            int[,] mx = Matrix.CopyMatrix();
+            fixed (byte* newStart = image, oldStart = original)
             {
-                fixed (byte* start = image, origStart = original)
+                byte* newPtr = newStart;
+                byte* oldPtr = oldStart;
+                for (int y = 0; y < image.Height; ++y)
                 {
-                    int width_3 = image.Width * 3;
-                    int h = image.Height;
-                    int x, y, nVal, stride = width_3;
-                    int[,] convMatrix = Matrix.CopyMatrix();
-
-                    // Iterate through rows
-                    for (y = 0; y < h; ++y)
+                    for (int x = 0; x < width_3; ++x)
                     {
-                        // Get rows
-                        byte* row = start + (y * stride);
-                        byte* rowOrig = origStart + (y * stride);
-                        // Iterate through columns
-                        for (x = 0; x < width_3; ++x)
-                        {
-                            byte mm = rowOrig[x];
-                            byte tl = y > 0 && x >= 3 ? rowOrig[x - stride - 3] : mm;
-                            byte tm = y > 0 ? rowOrig[x - stride] : mm;
-                            byte tr = y > 0 && x < width_3 - 3 ? rowOrig[x - stride + 3] : mm;
-                            byte ml = x >= 3 ? rowOrig[x - 3] : mm;
-                            byte mr = x < width_3 - 3 ? rowOrig[x + 3] : mm;
-                            byte bl = y < h - 1 && x >= 3 ? rowOrig[x + stride - 3] : mm;
-                            byte bm = y < h - 1 ? rowOrig[x + stride] : mm;
-                            byte br = y < h - 1 && x < width_3 - 3 ? rowOrig[x + stride + 3] : mm;
-                            nVal = (
-                                tl * convMatrix[0, 0] + tm * convMatrix[1, 0] + tr * convMatrix[2, 0] +
-                                ml * convMatrix[0, 1] + mm * convMatrix[1, 1] + mr * convMatrix[2, 1] +
-                                bl * convMatrix[0, 2] + bm * convMatrix[1, 2] + br * convMatrix[2, 2])
-                                / Matrix.Divisor + Matrix.Bias;
+                        byte mm = *oldPtr;
+                        byte tl = y > 0 && x >= 3 ? oldPtr[-width_3 - 3] : mm;
+                        byte tm = y > 0 ? oldPtr[-width_3] : mm;
+                        byte tr = y > 0 && x < width_3 - 3 ? oldPtr[-width_3 + 3] : mm;
+                        byte ml = x >= 3 ? oldPtr[-3] : mm;
+                        byte mr = x < width_3 - 3 ? oldPtr[3] : mm;
+                        byte bl = y < image.Height - 1 && x >= 3 ? oldPtr[width_3 - 3] : mm;
+                        byte bm = y < image.Height - 1 ? oldPtr[width_3] : mm;
+                        byte br = y < image.Height - 1 && x < width_3 - 3 ? oldPtr[width_3 + 3] : mm;
+                        float nVal = (
+                            tl * mx[0, 0] + tm * mx[1, 0] + tr * mx[2, 0] +
+                            ml * mx[0, 1] + mm * mx[1, 1] + mr * mx[2, 1] +
+                            bl * mx[0, 2] + bm * mx[1, 2] + br * mx[2, 2])
+                            / Matrix.Divisor + Matrix.Bias;
 
-                            row[x] = nVal.ClampToByte();
-                        }
-                        reporter?.Report(y, 0, h - 1);
+                        *newPtr = nVal.ClampToByte();
+                        ++newPtr;
+                        ++oldPtr;
                     }
+                    reporter?.Report(y, 0, image.Height - 1);
                 }
             }
             reporter?.Done();
