d2fdb8ebd36c3fdb1f6922b691db7601eea53705
hajduakos <sonic18291@hotmail.com>
2022-07-16 01:03:29 +0100

    Common base class for Prewitt and Sobel filters, fixes #54

---
+++ FilterLib/Filters/Edges/DualConvolutionBase.cs
@@ -0,0 +1,69 @@
+﻿using FilterLib.Filters.Other;
+using FilterLib.Reporting;
+using FilterLib.Util;
+
+namespace FilterLib.Filters.Edges
+{
+    /// <summary>
+    /// Base class for filters that perform two convolutions and combine them.
+    /// </summary>
+    public abstract class DualConvolutionBase : FilterInPlaceBase
+    {
+        private readonly ConvolutionFilter conv1, conv2;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="conv1">First convolution</param>
+        /// <param name="conv2">Second convolution</param>
+        public DualConvolutionBase(Conv3x3 conv1, Conv3x3 conv2)
+        {
+            this.conv1 = new(conv1);
+            this.conv2 = new(conv2);
+        }
+
+        /// <summary>
+        /// Combine two components after the convolutions.
+        /// </summary>
+        /// <param name="c1">First component</param>
+        /// <param name="c2">Second component</param>
+        /// <returns>Combination</returns>
+        public virtual byte Combine(int c1, int c2) => System.MathF.Sqrt(c1 * c1 + c2 * c2).ClampToByte();
+
+        /// <inheritdoc/>
+        public override sealed unsafe void ApplyInPlace(Image image, IReporter reporter = null)
+        {
+            reporter?.Start();
+            Image tmp = (Image)image.Clone();
+
+            conv1.ApplyInPlace(tmp, new SubReporter(reporter, 0, 33, 0, 100));
+            conv2.ApplyInPlace(image, new SubReporter(reporter, 34, 66, 0, 100));
+            IReporter subRep = new SubReporter(reporter, 67, 100, 0, 100);
+
+            // Cache all different combinations
+            byte[,] map = new byte[256, 256];
+            for (int x = 0; x <= 255; x++)
+                for (int y = 0; y < 256; y++)
+                    map[x, y] = Combine(x, y);
+
+            int width_3 = image.Width * 3;
+            fixed (byte* imgStart = image, tmpStart = tmp)
+            {
+                byte* imgPtr = imgStart;
+                byte* tmpPtr = tmpStart;
+                for (int y = 0; y < image.Height; ++y)
+                {
+                    for (int x = 0; x < width_3; ++x)
+                    {
+                        *imgPtr = map[*imgPtr, *tmpPtr];
+                        ++imgPtr;
+                        ++tmpPtr;
+                    }
+                    subRep.Report(y, 0, image.Height - 1);
+                }
+
+            }
+            reporter?.Done();
+        }
+    }
+}
--- FilterLib/Filters/Edges/PrewittFilter.cs
+++ FilterLib/Filters/Edges/PrewittFilter.cs
@@ -1,58 +1,17 @@
-﻿using FilterLib.Filters.Other;
-using FilterLib.Reporting;
-using FilterLib.Util;
-
-namespace FilterLib.Filters.Edges
+﻿namespace FilterLib.Filters.Edges
 {
     /// <summary>
     /// Prewitt filter combining two convolutions.
     /// </summary>
     [Filter]
-    public sealed class PrewittFilter : FilterInPlaceBase
+    public sealed class PrewittFilter : DualConvolutionBase
     {
-        private readonly ConvolutionFilter conv1 =
-            new(new Conv3x3(-1, -1, -1, 0, 0, 0, 1, 1, 1));
-
-        private readonly ConvolutionFilter conv2 =
-            new(new Conv3x3(-1, 0, 1, -1, 0, 1, -1, 0, 1));
-
-        /// <inheritdoc/>
-        public override unsafe void ApplyInPlace(Image image, IReporter reporter = null)
-        {
-            reporter?.Start();
-            // Clone image
-            Image tmp = (Image)image.Clone();
-
-            // Apply vertical convolution
-            conv1.ApplyInPlace(tmp, new SubReporter(reporter, 0, 33, 0, 100));
-            // Apply horizontal convolution
-            conv2.ApplyInPlace(image, new SubReporter(reporter, 34, 66, 0, 100));
-            IReporter subRep = new SubReporter(reporter, 67, 100, 0, 100);
-
-            // Cache all different combinations
-            byte[,] map = new byte[256, 256];
-            for (int x = 0; x < 256; x++)
-                for (int y = 0; y < 256; y++)
-                    map[x, y] = System.MathF.Sqrt(x * x + y * y).ClampToByte();
-
-            int width_3 = image.Width * 3;
-            fixed (byte* imgStart = image, tmpStart = tmp)
-            {
-                byte* imgPtr = imgStart;
-                byte* tmpPtr = tmpStart;
-                for (int y = 0; y < image.Height; ++y)
-                {
-                    for (int x = 0; x < width_3; ++x)
-                    {
-                        *imgPtr = map[*imgPtr, *tmpPtr];
-                        ++imgPtr;
-                        ++tmpPtr;
-                    }
-                    subRep.Report(y, 0, image.Height - 1);
-                }
-
-            }
-            reporter?.Done();
-        }
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public PrewittFilter() : base(
+            new(-1, -1, -1, 0, 0, 0, 1, 1, 1),
+            new(-1, 0, 1, -1, 0, 1, -1, 0, 1))
+        { }
     }
 }
