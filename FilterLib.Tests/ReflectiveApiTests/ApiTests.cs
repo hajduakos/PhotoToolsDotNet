2c91d0c5ede87d5268d76858849d662606788ae5
hajduakos <sonic18291@hotmail.com>
2022-07-23 23:31:48 +0100

    Add some API tests

--- FilterLib.Tests/ReflectiveApiTests/ApiTests.cs
+++ FilterLib.Tests/ReflectiveApiTests/ApiTests.cs
@@ -13,5 +13,21 @@ namespace FilterLib.Tests.ReflectiveApiTests
         public void TestListBlends() =>
             Assert.AreEqual(26, ReflectiveApi.GetBlendTypes().Count());
 
+        [Test]
+        public void TestFilterExists() =>
+            Assert.IsTrue(ReflectiveApi.CheckFilterExists(typeof(Filters.Color.InvertFilter).Name));
+
+        [Test]
+        public void TestFilterNotExists() =>
+            Assert.IsFalse(ReflectiveApi.CheckFilterExists("This should not exist..."));
+
+        [Test]
+        public void TestBlendExists() =>
+            Assert.IsTrue(ReflectiveApi.CheckBlendExists(typeof(Blending.Normal.NormalBlend).Name));
+
+        [Test]
+        public void TestBlendNotExists() =>
+            Assert.IsFalse(ReflectiveApi.CheckBlendExists("This should not exist..."));
+
     }
 }
