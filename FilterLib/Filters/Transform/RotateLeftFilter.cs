9bda5016a6574a62b2a873369e68afc871f71279
hajduakos <sonic18291@hotmail.com>
2022-08-02 22:38:47 +0100

    Parallel rotate left and right filters

--- FilterLib/Filters/Transform/RotateLeftFilter.cs
+++ FilterLib/Filters/Transform/RotateLeftFilter.cs
@@ -1,4 +1,5 @@
 ﻿using FilterLib.Reporting;
+using Parallel = System.Threading.Tasks.Parallel;
 
 namespace FilterLib.Filters.Transform
 {
@@ -12,23 +13,27 @@ namespace FilterLib.Filters.Transform
         public override unsafe Image Apply(Image image, IReporter reporter = null)
         {
             reporter?.Start();
+            object reporterLock = new();
+            int progress = 0;
             Image rotated = new(image.Height, image.Width);
             int oldWidth_3 = image.Width * 3;
             int newWidth_3 = rotated.Width * 3;
             fixed (byte* oldStart = image, newStart = rotated)
             {
-                for (int y = 0; y < image.Height; ++y)
+                byte* newStart0 = newStart;
+                byte* oldStart0 = oldStart;
+                Parallel.For(0, image.Height, y =>
                 {
-                    byte* oldRow = oldStart + y * oldWidth_3;
+                    byte* oldRow = oldStart0 + y * oldWidth_3;
                     for (int x = 0; x < oldWidth_3; x += 3)
                     {
                         int idx = (rotated.Height - x / 3 - 1) * newWidth_3 + y * 3;
-                        newStart[idx] = oldRow[x];
-                        newStart[idx + 1] = oldRow[x + 1];
-                        newStart[idx + 2] = oldRow[x + 2];
+                        newStart0[idx] = oldRow[x];
+                        newStart0[idx + 1] = oldRow[x + 1];
+                        newStart0[idx + 2] = oldRow[x + 2];
                     }
-                    reporter?.Report(y + 1, 0, image.Height);
-                }
+                    if (reporter != null) lock (reporterLock) reporter.Report(++progress, 0, image.Height);
+                });
             }
             reporter?.Done();
             return rotated;
