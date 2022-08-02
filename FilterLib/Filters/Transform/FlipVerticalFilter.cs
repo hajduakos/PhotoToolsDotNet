7910e2a6e77e97be306f91ce83fed281b88074ed
hajduakos <sonic18291@hotmail.com>
2022-08-02 22:17:58 +0100

    Parallel flip vertical filter

--- FilterLib/Filters/Transform/FlipVerticalFilter.cs
+++ FilterLib/Filters/Transform/FlipVerticalFilter.cs
@@ -1,4 +1,5 @@
 ﻿using FilterLib.Reporting;
+using Parallel = System.Threading.Tasks.Parallel;
 
 namespace FilterLib.Filters.Transform
 {
@@ -12,20 +13,23 @@ namespace FilterLib.Filters.Transform
         public override unsafe void ApplyInPlace(Image image, IReporter reporter = null)
         {
             reporter?.Start();
+            object reporterLock = new();
+            int progress = 0;
             int width_3 = image.Width * 3;
             int height_div2 = image.Height / 2;
             fixed (byte* start = image)
             {
-                for (int y = 0; y < height_div2; ++y)
+                byte* start0 = start;
+                Parallel.For(0, height_div2, y =>
                 {
-                    byte* row1 = start + y * width_3;
-                    byte* row2 = start + (image.Height - y - 1) * width_3;
+                    byte* row1 = start0 + y * width_3;
+                    byte* row2 = start0 + (image.Height - y - 1) * width_3;
                     for (int x = 0; x < width_3; ++x)
                     {
                         (row2[x], row1[x]) = (row1[x], row2[x]);
                     }
-                    reporter?.Report(y + 1, 0, height_div2);
-                }
+                    if (reporter != null) lock (reporterLock) reporter.Report(++progress, 0, height_div2);
+                });
             }
             reporter?.Done();
         }
