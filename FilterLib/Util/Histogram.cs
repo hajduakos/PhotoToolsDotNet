8111a752b9f58e3033514cd54a9753f63839b7b0
hajduakos <sonic18291@hotmail.com>
2022-08-07 22:08:38 +0100

    Parallel histogram computation

--- FilterLib/Util/Histogram.cs
+++ FilterLib/Util/Histogram.cs
@@ -1,7 +1,10 @@
-﻿namespace FilterLib.Util
+﻿using Parallel = System.Threading.Tasks.Parallel;
+
+namespace FilterLib.Util
 {
     public static class Histogram
     {
+        const int MAX_THREADS = 16;
         /// <summary>
         /// Get luminance histogram corresponding to an image.
         /// </summary>
@@ -9,24 +12,41 @@
         /// <returns>Luminance histogram (256 elements)</returns>
         public unsafe static int[] GetLuminanceHistogram(Image image)
         {
-            // Array containing histogram values
-            int[] histogram = new int[256];
-            for (int i = 0; i < 256; ++i) histogram[i] = 0;
+            // Divide into equal blocks for each thread (except the last which has the remainder)
+            int threads = System.Math.Min(image.Height, MAX_THREADS);
+            int blockSize = image.Height / threads;
+            // Each thread has local data to avoid locking
+            int[,] histogram = new int[threads, 256];
+            Parallel.For(0, threads, t => { for (int i = 0; i < 256; ++i) histogram[t, i] = 0; });
 
             fixed (byte* start = image)
             {
-                byte* ptr = start;
-                for (int y = 0; y < image.Height; ++y)
+                byte* start0 = start;
+                Parallel.For(0, threads, t =>
                 {
-                    for (int x = 0; x < image.Width; ++x)
+                    int yStart = t * blockSize;
+                    int yEnd = t == threads - 1 ? image.Height : (t + 1) * blockSize;
+                    byte* ptr = start0 + yStart * image.Width * 3;
+                    for (int y = yStart; y < yEnd; ++y)
                     {
-                        ++histogram[(byte)RGB.GetLuminance(ptr[0], ptr[1], ptr[2])];
-                        ptr += 3;
+                        for (int x = 0; x < image.Width; ++x)
+                        {
+                            byte lum = (byte)RGB.GetLuminance(ptr[0], ptr[1], ptr[2]);
+                            ++histogram[t, lum];
+                            ptr += 3;
+                        }
                     }
-                }
+                });
 
             }
-            return histogram;
+            // Finally merge the results from each thread
+            int[] merged = new int[256];
+            Parallel.For(0, 256, i =>
+            {
+                merged[i] = 0;
+                for (int t = 0; t < threads; ++t) merged[i] += histogram[t, i];
+            });
+            return merged;
         }
     }
 }
